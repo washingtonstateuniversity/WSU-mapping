9954fdf42671bb12630d0c3e27e4ce89d710cd19
jeremyBass <jeremybass@cableone.net>
2012-07-26 00:18:50 +0000

    git-svn-id: http://urelweb-01.it.wsu.edu/svn/campusMap@379 2bf17667-1719-456c-86a0-23e81fbc8e4e

--- branches/CampusMap2010/CampusMap2010/Content/js/init_test.js
+++ branches/CampusMap2010/CampusMap2010/Content/js/init_test.js
@@ -929,6 +929,26 @@ $(document).ready(function(){
 					$('#main_nav a[href$="'+startingUrl.split('=')[1]+'"]').trigger('click');
 				}
 			}
+			
+(function($) { 
+  $.fn.nodoubletapzoom = function() { 
+      $(this).bind('touchstart', function preventZoom(e) { 
+        var t2 = e.timeStamp 
+          , t1 = $(this).data('lastTouch') || t2 
+          , dt = t2 - t1 
+          , fingers = e.originalEvent.touches.length; 
+        $(this).data('lastTouch', t2); 
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap 
+ 
+        e.preventDefault(); // double tap - prevent the zoom 
+        // also synthesize click events we just swallowed up 
+        $(this).trigger('click').trigger('click'); 
+      }); 
+  }; 
+})(jQuery); 
+if($(window).width()<=404)$('#centralMap').nodoubletapzoom()
+			
+			
 		});
 		if($('#centralMap').length){
 			$(window).resize(function(){resizeBg($('.central_layout.public.central #centralMap'),160,($(window).width()<=600?($(window).width()<=404?0:155):201)+listOffset)}).trigger("resize");
