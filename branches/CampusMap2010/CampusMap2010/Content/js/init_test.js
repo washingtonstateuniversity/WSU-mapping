bd9c5bad173dbb1533ea4db7967512963bd6f4fb
jeremyBass <jeremybass@cableone.net>
2012-08-21 20:45:58 +0000

    git-svn-id: http://urelweb-01.it.wsu.edu/svn/campusMap@422 2bf17667-1719-456c-86a0-23e81fbc8e4e

--- branches/CampusMap2010/CampusMap2010/Content/js/init_test.js
+++ branches/CampusMap2010/CampusMap2010/Content/js/init_test.js
@@ -587,7 +587,8 @@ function addShapeToMap(jObj,i,shape){
 				 jObj.gmap('setOptions',shape.style.events.mouseover,this);
 				// if(style.mouseover.callback)style.mouseover.callback();
 				if(typeof(shape.style.events.mouseover.onEnd)!="undefined" && shape.style.events.mouseover.onEnd != ""){
-					eval(shape.style.events.mouseover.onEnd.replace("\u0027","'"));
+					var myCode = shape.style.events.mouseover.onEnd.replace("\u0027","'");
+					var myFucn = new Function(myCode); myFucn();
 				}		
 			 }
 		}).mouseout(function(){
