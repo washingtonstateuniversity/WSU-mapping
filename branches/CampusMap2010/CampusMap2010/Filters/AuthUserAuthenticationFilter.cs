40dc26ea6eb3c183397cda02d5229882ab245808
jeremyBass <jeremybass@cableone.net>
2012-10-29 16:47:39 +0000

    git-svn-id: http://urelweb-01.it.wsu.edu/svn/campusMap@492 2bf17667-1719-456c-86a0-23e81fbc8e4e

--- branches/CampusMap2010/CampusMap2010/Filters/AuthUserAuthenticationFilter.cs
+++ branches/CampusMap2010/CampusMap2010/Filters/AuthUserAuthenticationFilter.cs
@@ -45,7 +45,7 @@ namespace campusMap.Filters
                 // Not authenticated, redirect to login
                 String username = Authentication.authenticate();
 
-                users[] authors = ActiveRecordBase<users>.FindAllByProperty("Nid", username);
+                users[] authors = ActiveRecordBase<users>.FindAllByProperty("nid", username);
 
                 if (authors.Length == 0)
                 {
