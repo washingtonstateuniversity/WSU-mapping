7d05460df93fa46a46cb295b73dc48a57056fdd4
jeremyBass <jeremybass@cableone.net>
2013-10-10 17:59:23 +0000

    git-svn-id: http://urelweb-01.it.wsu.edu/svn/campusMap@583 2bf17667-1719-456c-86a0-23e81fbc8e4e

--- branches/CampusMap2010/CampusMap2010/Content/js/map_ini.js
+++ branches/CampusMap2010/CampusMap2010/Content/js/map_ini.js
@@ -38,15 +38,22 @@ $(function(){
 			iniMap("",setup);
 		}
 		if(mapInst.length){
+							var mHeight = ($(window).height()<=404?0:130);
+				if($('.embeded').length)mHeight = 0;
+				if($('.layoutfree').length)mHeight = 93;
 			$(window).resize(function(){
 				//alert( ($('.embeded').length||$('.layoutfree').length) ?0:130 );
 				// can't us mapInst here cause it's still in the admin area.  Split that.
-				resizeBg($('.central_layout.public.central #centralMap'), ( ($('.embeded').length||$('.layoutfree').length) ?0:($(window).height()<=404?0:130)),($('.embeded').length?0:($(window).width()<=404?0:($(window).width()<=600?155:201))) + $('#selectedPlaceList').width())
+
+				
+				resizeBg($('.central_layout.public.central #centralMap'), ( mHeight),($('.embeded').length?0:($(window).width()<=404?0:($(window).width()<=600?155:201))) + $('#selectedPlaceList').width())
 			}).trigger("resize");
 			$(window).resize(function(){
 				// can't us mapInst here cause it's still in the admin area.  Split that.
+
+				
 				$('#navwrap').height($('#centralMap_wrap').height()-70);
-				resizeBg($('.cAssest'),( ($('.embeded').length||$('.layoutfree').length) ?0:130));
+				resizeBg($('.cAssest'),mHeight);
 				
 				reset_Dirscrollbar();
 				reset_Listscrollbar();
