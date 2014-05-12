ad6e1e671914b10585db9fa9fd4129f5f23f235f
nathan_owen <nathan_owen@2bf17667-1719-456c-86a0-23e81fbc8e4e>
2014-05-12 18:03:13 +0000

    git-svn-id: http://urelweb-01.it.wsu.edu/svn/campusMap@621 2bf17667-1719-456c-86a0-23e81fbc8e4e

--- branches/CampusMap2010/CampusMap2010/Controllers/placeController.cs
+++ branches/CampusMap2010/CampusMap2010/Controllers/placeController.cs
@@ -63,7 +63,7 @@ namespace campusMap.Controllers {
                 campus.gameDayTourOn = true;
             if (toggleoff)
                 campus.gameDayTourOn = false;
-            PropertyBag["percents"] = new string[] { "0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+            PropertyBag["percents"] = new string[] { "0%", "5%", "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%", "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%"};
             PropertyBag["places"] = placeservice.getPlacesByKeyword("Game Day Parking - General");
             PropertyBag["campus"] = ActiveRecordBase<campus>.Find(1);
         }
