ffc26d64058d96c5033164859f14de9bce0b0b02
Jeremy Bass <jeremybass@cableone.net>
2015-01-13 09:42:34 -0800

    one to many ini of the map

--- CampusMap2010/Content/src/js/wsu_maps.js
+++ CampusMap2010/Content/src/js/wsu_maps.js
@@ -263,50 +263,50 @@ var startingUrl=startingUrl||null;
 			//});
 		},
 		updateMap:function (jObj,_load,showSum,callback){
-		if(typeof(_load)==='undefined'){
-			_load = false;
-		}
-		if(typeof(showSum)==='undefined'){
-			showSum = false;
-		}
-		if(typeof(callback)==='undefined'){
-			callback = false;
-		}
-		$.wsu_maps.state.cur_mid = 0;
-		$.wsu_maps.state.cur_nav = _load;
-		var url="";
-		if($.isNumeric(_load)){
-			url=$.wsu_maps.state.siteroot+"public/get_place.castle";
-		}else{
-			url=$.wsu_maps.state.siteroot+"public/get_place_by_category.castle";	
-		}
-		$.getJSON(url+'?callback=?'+(_load!==false && !$.isNumeric(_load)?'&cat[]='+_load:($.isNumeric(_load)?'&id='+_load:'')), function(data) {
-			if(!$.isNumeric(_load)){
-				$.wsu_maps.general.autoOpenListPanel();
+			if(typeof(_load)==='undefined'){
+				_load = false;
 			}
-			var cleanedData = [];
-			cleanedData.markers = [];
-			if(typeof(data.markers)!=='undefined' &&  !$.isEmptyObject( data.markers )){
-				$.each( data.markers, function(i, marker) {
-					if($.isNumeric(_load)){
-						marker.bounds=true;
-					}
-					if(typeof(marker.id)==='undefined'){
-						delete data.markers[i];
-					}else if(typeof(marker.error)!=='undefined'){
-						delete data.markers[i]; 
-					}else{
-						cleanedData.markers.push(data.markers[i]);		
-					}
-				});
-				if(typeof(cleanedData)!=='undefined'){
-					$.wsu_maps.general.loadData(jObj,cleanedData,callback);
+			if(typeof(showSum)==='undefined'){
+				showSum = false;
+			}
+			if(typeof(callback)==='undefined'){
+				callback = false;
+			}
+			$.wsu_maps.state.cur_mid = 0;
+			$.wsu_maps.state.cur_nav = _load;
+			var url="";
+			if($.isNumeric(_load)){
+				url=$.wsu_maps.state.siteroot+"public/get_place.castle";
+			}else{
+				url=$.wsu_maps.state.siteroot+"public/get_place_by_category.castle";	
+			}
+			$.getJSON(url+'?callback=?'+(_load!==false && !$.isNumeric(_load)?'&cat[]='+_load:($.isNumeric(_load)?'&id='+_load:'')), function(data) {
+				if(!$.isNumeric(_load)){
+					$.wsu_maps.general.autoOpenListPanel();
 				}
-				$.wsu_maps.general.loadListings(cleanedData,showSum);
-			}
-			$.wsu_maps.general.prep_html();
-		});
-	},
+				var cleanedData = [];
+				cleanedData.markers = [];
+				if(typeof(data.markers)!=='undefined' &&  !$.isEmptyObject( data.markers )){
+					$.each( data.markers, function(i, marker) {
+						if($.isNumeric(_load)){
+							marker.bounds=true;
+						}
+						if(typeof(marker.id)==='undefined'){
+							delete data.markers[i];
+						}else if(typeof(marker.error)!=='undefined'){
+							delete data.markers[i]; 
+						}else{
+							cleanedData.markers.push(data.markers[i]);		
+						}
+					});
+					if(typeof(cleanedData)!=='undefined'){
+						$.wsu_maps.general.loadData(jObj,cleanedData,callback);
+					}
+					$.wsu_maps.general.loadListings(cleanedData,showSum);
+				}
+				$.wsu_maps.general.prep_html();
+			});
+		},
 
 		addCentralControlls:function (){
 			if($('.mapControl').length===0){
--- CampusMap2010/Content/src/js/wsu_maps.responsive.js
+++ CampusMap2010/Content/src/js/wsu_maps.responsive.js
@@ -47,5 +47,4 @@
 			}
 		},
 	};
-	$.wsu_maps.ini();
 })(jQuery);
