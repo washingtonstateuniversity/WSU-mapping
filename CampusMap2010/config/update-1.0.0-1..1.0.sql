b3b6833666d2ae393962c2f4fc9e674af5bce3d8
Jeremy Bass <jeremybass@cableone.net>
2015-01-16 11:58:57 -0800

    provide the upgrade sql from 1.0.0 to 1.1.0

---
+++ CampusMap2010/config/update-1.0.0-1..1.0.sql
@@ -0,0 +1,26 @@
+﻿    if exists (select 1 from sysobjects where id = OBJECT_ID(N'[FKAA44A29FEA53B1C0]') AND parent_obj = OBJECT_ID('geometrics_to_geoparents'))
+ALTER TABLE geometrics_to_geoparents  drop constraint FKAA44A29FEA53B1C0
+    if exists (select 1 from sysobjects where id = OBJECT_ID(N'[FK36ED1C56214520DC]') AND parent_obj = OBJECT_ID('geometrics_to_geoparents'))
+ALTER TABLE geometrics_to_geoparents  drop constraint FK36ED1C56214520DC
+if exists (select * from dbo.sysobjects where id = object_id(N'geometrics_to_geoparents') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table geometrics_to_geoparents
+
+
+    CREATE TABLE geometrics_to_geoparents (
+        parent_geometric_id INT not null,
+       geometric_id INT not null
+    )
+
+	
+	ALTER TABLE geometrics
+		ADD parent INT null
+
+    ALTER TABLE geometrics 
+        add constraint FKAA44A29FEA53B1C0 
+        foreign key (parent) 
+        references geometrics
+
+
+    ALTER TABLE geometrics_to_geoparents 
+        add constraint FK36ED1C56214520DC 
+        foreign key (parent_geometric_id) 
+        references geometrics
