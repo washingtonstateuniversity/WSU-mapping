485e46d7dd7bcbee77d54f2df06d4de7453fa01e
Jeremy Bass <jeremybass@cableone.net>
2015-03-12 15:44:56 -0700

    implement an embeding test section, under /public/test.castle

--- CampusMap2010/Controllers/publicController.cs
+++ CampusMap2010/Controllers/publicController.cs
@@ -1157,16 +1157,14 @@ where p.status = 3
             sendPlaceJson(items, callback);
         }
 
-        public void gamedaytourmode()
-        {
+        public void gamedaytourmode()  {
             Hashtable result = new Hashtable();
             campus thecampus = ActiveRecordBase<campus>.Find(1);
             result["mode"] = thecampus.gameDayTourOn;
             RenderText( JsonConvert.SerializeObject(result) );
         }
 
-        public void get_place_by_categoryname(string catname, string callback)
-        {
+        public void get_place_by_categoryname(string catname, string callback) {
             CancelView();
             CancelLayout();
             place[] items = new List<place>().ToArray();
@@ -1200,7 +1198,10 @@ where p.status = 3
             RenderText(json);
         }
 
+
+
         public void markerSVG(int idx) {
+            CancelView();
             String json = "<?xml version='1.0' encoding='utf-8'?><!-- Generator: Adobe Illustrator 18.1.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  --><svg version='1.1' id='Layer_2' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='104 23 429.5 739' enable-background='new 104 23 429.5 739' xml:space='preserve'><path fill='#9E1D32' stroke='#FFFFFF' stroke-width='6' stroke-miterlimit='10' d='M529.7,237.8c0,116.1-196.6,521.1-210.4,521.1 c-16.9,0-210.4-404.8-210.4-521.1S203,27.4,319.3,27.4S529.7,121.5,529.7,237.8z'/><text class='marker-number' transform='matrix(1 0 0 1 133.6372 294.8559)' x='180' y='0' text-anchor='middle'  fill='#F1F1F1' font-size='225' >" + idx + "</text></svg>";
             Response.ContentType = "image/svg+xml; charset=UTF-8";
             RenderText(json);
@@ -1617,6 +1618,9 @@ where p.status = 3
         }
         #endregion
 
+        [Layout("test")]
+        public void test() {
+        }
 
         #region Advertisement  NOTE: needs work
         public void IncClicked(int id) {
