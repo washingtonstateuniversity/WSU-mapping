ebe8e48b40ac8b43cbd6895a7e97d0836d1fd227
Jeremy Bass <jeremybass@cableone.net>
2015-03-12 14:11:05 -0700

    make sure that we are highlighting the markes when an IW is opened regardlesss of trigger

--- CampusMap2010/Content/src/js/wsu_maps.infobox.js
+++ CampusMap2010/Content/src/js/wsu_maps.infobox.js
@@ -231,6 +231,8 @@
 			if($('#taby'+i).length){
 				$('#taby'+i).tabs('destroy').tabs();
 			}
+			$.wsu_maps.markers.unhighlight_marker($.wsu_maps.state.markerLog[i]);
+			$.wsu_maps.state.active_marker = null;
 		},
 		
 		open_IW:function(jObj,marker,i){			
@@ -262,8 +264,11 @@
 			});
 			$.wsu_maps.state.ib[i].rePosition();
 			$.wsu_maps.state.ibHover =  false;
-
-
+			if($.wsu_maps.state.active_marker !== null){
+				$.wsu_maps.markers.unhighlight_marker($.wsu_maps.state.active_marker);
+			}
+			$.wsu_maps.markers.highlight_marker($.wsu_maps.state.markerLog[i]);
+			$.wsu_maps.state.active_marker = $.wsu_maps.state.markerLog[i];
 			
 			//$.wsu_maps.general.prep_html();
 		},
