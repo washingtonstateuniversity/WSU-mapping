58163360ddf149b02658838df8edbf45e4262bbd
Jeremy Bass <jeremybass@cableone.net>
2015-04-06 13:04:45 -0700

    refactor resize for new spine base .. alsono reason to do 2 back to back resize triggers

--- CampusMap2010/Content/src/js/wsu_maps.frontend.js
+++ CampusMap2010/Content/src/js/wsu_maps.frontend.js
@@ -18,32 +18,32 @@
 					$.wsu_maps.iniMap("",$.wsu_maps.setup);
 				}
 				if($.wsu_maps.state.map_jObj.length){
-					var mHeight = $('#header_bar').outerHeight();//($(window).height()<=404?0:$('#header_bar').outerHeight());
-					if($('.embeded').length){
-						mHeight = 0;
-					}
-					if($('.layoutfree').length){
-						mHeight = 93;
-					}
+					
 					$(window).resize(function(){
 						//alert( ($('.embeded').length||$('.layoutfree').length) ?0:130 );
 						// can't us map_jObj here cause it's still in the admin area.  Split that.
-		
+						var mHeight = $('#header_bar').outerHeight();//($(window).height()<=404?0:$('#header_bar').outerHeight());
+						if($('.embeded').length){
+							mHeight = 0;
+						}
+						if($('.layoutfree').length){
+							mHeight = 93;
+						}
+						
+						if($("#spine.shelved").length){
+							mHeight = $('.spine-header').outerHeight() + $('#header_bar').outerHeight();
+						}
 						var map_obj = $('.central_layout.public.central #centralmap');
-						var win_w = $(window).width();
-						var mWidth = ( $('.embeded').length?0:( win_w<=404?0:(win_w<=600?155:201) ) ) + $('#selectedPlaceList').width();
-						$.wsu_maps.responsive.resizeBg(map_obj, mHeight, mWidth);
-					}).trigger("resize");
-					$(window).resize(function(){
+						//var win_w = $(window).width();
+						//var mWidth = $('main').width();//( $('.embeded').length?0:( win_w<=404?0:(win_w<=600?155:201) ) ) + $('#selectedPlaceList').width();
+						//$.wsu_maps.responsive.resizeBg(map_obj, mHeight, mWidth);
+						
+						map_obj.height($(window).height()-mHeight);
+						map_obj.width($('main').width());
+
 						// can't us map_jObj here cause it's still in the admin area.  Split that.
-		
-						
-						
+						mHeight = $('#header_bar').outerHeight();
 						$.wsu_maps.responsive.resizeBg($('.cAssest'),mHeight);
-						
-						
-						//if($('#listing').length)setup_scrollbar($('#listing'));
-						
 					}).trigger("resize");
 				}
 				$('#directionsTo').hide();
