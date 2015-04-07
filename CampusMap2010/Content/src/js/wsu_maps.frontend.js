f84ff66510eea2f8c6f3487d432021b629ba9fcb
Jeremy Bass <jeremybass@cableone.net>
2015-04-07 11:21:16 -0700

    clean up of cruft

--- CampusMap2010/Content/src/js/wsu_maps.frontend.js
+++ CampusMap2010/Content/src/js/wsu_maps.frontend.js
@@ -20,8 +20,6 @@
 				if($.wsu_maps.state.map_jObj.length){
 					
 					$(window).resize(function(){
-						//alert( ($('.embeded').length||$('.layoutfree').length) ?0:130 );
-						// can't us map_jObj here cause it's still in the admin area.  Split that.
 						var mHeight = $('#header_bar').outerHeight();//($(window).height()<=404?0:$('#header_bar').outerHeight());
 						if($('.embeded').length){
 							mHeight = 0;
@@ -34,14 +32,10 @@
 							mHeight = $('.spine-header').outerHeight() + $('#header_bar').outerHeight();
 						}
 						var map_obj = $('.central_layout.public.central #centralmap');
-						//var win_w = $(window).width();
-						//var mWidth = $('main').width();//( $('.embeded').length?0:( win_w<=404?0:(win_w<=600?155:201) ) ) + $('#selectedPlaceList').width();
-						//$.wsu_maps.responsive.resizeBg(map_obj, mHeight, mWidth);
-						
+
 						map_obj.height($(window).height()-mHeight);
 						map_obj.width($('main').width());
 
-						// can't us map_jObj here cause it's still in the admin area.  Split that.
 						mHeight = $('#header_bar').outerHeight();
 						$.wsu_maps.responsive.resizeBg($('.cAssest'),mHeight);
 					}).trigger("resize");
